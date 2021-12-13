abd1c67a8f8681bb521399b85cab9d8502d23cfc
Aghyad <zenlogie@gmail.com>
2021-12-13 23:11:47 +0100

    redux store in index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import { store } from "./store";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { tasksReducer } from "./store/reducers";
+
+export const store = createStore(tasksReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
